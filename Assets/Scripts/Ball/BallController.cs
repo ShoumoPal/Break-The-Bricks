c4932504e7f5feb709f088a9700e4bdfb5f0bbbb
ShoumoPal <arka.pal21@gmail.com>
2023-08-17 19:18:51 +0530

    Added a tutorial section in lobby screen

--- Assets/Scripts/Ball/BallController.cs
+++ Assets/Scripts/Ball/BallController.cs
@@ -4,12 +4,26 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private Transform shooterLocation;
     [SerializeField] private float returnSpeed;
+    private Rigidbody2D rb;
+    private bool isGrounded;
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        isGrounded = false;
+    }
+    private void Update()
+    {
+        if(rb.velocity.y == 0 && !isGrounded)
+        {
+            rb.velocity = Vector3.down;
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            isGrounded = true;
             rb.velocity = Vector3.zero;
             Vector3 direction = (shooterLocation.position - transform.position).normalized;
             direction.y = 0;
--- Assets/Scripts/Levels/LevelController.cs
+++ Assets/Scripts/Levels/LevelController.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelController : MonoBehaviour
 {
-    private int numOfBricks;
+    public int numOfBricks;
     private bool gameOver;
     [SerializeField] private bool levelComplete;
     [SerializeField] GameObject brickGrid;
@@ -13,7 +14,7 @@ public class LevelController : MonoBehaviour
     {
         gameOver = false;
         levelComplete = false;
-        numOfBricks = LevelManager.Instance.GetNumberOfBricks(1);
+        numOfBricks = LevelManager.Instance.GetNumberOfBricks(SceneManager.GetActiveScene().buildIndex);
     }
     private void Update()
     {
